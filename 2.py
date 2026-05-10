dc24b592767907e0606efce471d166507155476f
mrbeast207 <nmducdeptrai2701@gmail.com>
2026-05-10 10:56:27 +0700

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,9 @@
+name = "Nguyen Minh Duc"
+age = 19
+hometown = "Ha Noi"
+email = "mduc@gmail.com"
+
+print(name, type(name))
+print(age, type(age))
+print(hometown, type(hometown))
+print(email, type(email))
